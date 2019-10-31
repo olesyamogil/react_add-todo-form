b5d5221872ca024ad0c78b545a424b89a48ddccb
olesyamogil <olesya.mogil@gmail.com>
2019-10-31 10:16:51 +0200

    Updates

---
+++ precache-manifest.6b59f73d3760f14090fdb9f9a9f531d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49bb1b1697e6994249e5ddb4077579e",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "2228c938fb35fc2cdbc2",
+    "url": "/react_add-todo-form/static/css/main.b865b872.chunk.css"
+  },
+  {
+    "revision": "f8f02e1f768454e3da7a",
+    "url": "/react_add-todo-form/static/js/2.4b7a3035.chunk.js"
+  },
+  {
+    "revision": "2228c938fb35fc2cdbc2",
+    "url": "/react_add-todo-form/static/js/main.a19304b6.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
